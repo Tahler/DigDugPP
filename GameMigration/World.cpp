e379b91ea7d70e59d28256bd65070cf3b0e67e1b
Tyler Berry <tyler@berryac.com>
2015-05-01 12:59:06 -0600

    Only draws inside the window box

--- GameMigration/World.cpp
+++ GameMigration/World.cpp
@@ -6,12 +6,12 @@
 using Physics::Vector;
 
 // Measured in blocks
-const int WORLD_WIDTH = 30;
-const int WORLD_HEIGHT = 80;
+const int WORLD_WIDTH = 24;
+const int WORLD_HEIGHT = 16;
 
 World::World()
 {
-	blocks = vector<vector<Block*>>(WORLD_WIDTH, vector<Block*>(16));
+	blocks = vector<vector<Block*>>(WORLD_WIDTH, vector<Block*>(WORLD_HEIGHT));
 	window = Window(Point(0, 0));
 	init();
 }
@@ -71,35 +71,48 @@ void World::update()
 }
 void World::draw(Core::Graphics& g)
 {
-	for (unsigned int i = 0; i < blocks.size(); i++)
-	{
-		for (unsigned int j = 0; j < blocks[0].size(); j++)
-		{
-			if (window.box.contains(blocks[i][j]->a)) blocks[i][j]->draw(g);
-		}
-	}
-	//Block* upperLeft = &getBlockAt(window.box.a);
-	//Block* lowerRight = &getBlockAt(window.box.b);
-
-	//Point begin = Point(upperLeft->a.x / BLOCK_SIZE, upperLeft->a.y / BLOCK_SIZE);
-	//Point end = Point(lowerRight->b.x / BLOCK_SIZE, lowerRight->b.y / BLOCK_SIZE);
-	//
-	//for (unsigned int i = begin.x; i < end.x; i++)
+	// Draw all blocks //
+	//for (unsigned int i = 0; i < blocks.size(); i++)
 	//{
-	//	for (unsigned int j = begin.y; j < end.y; j++)
+	//	for (unsigned int j = 0; j < blocks[0].size(); j++)
 	//	{
 	//		blocks[i][j]->draw(g);
 	//	}
 	//}
+
+	// Check if block is on screen, if it is, draw it // 
+	//for (unsigned int i = 0; i < blocks.size(); i++)
+	//{
+	//	for (unsigned int j = 0; j < blocks[0].size(); j++)
+	//	{
+	//		if (blocks[i][j]->intersects(window.box)) blocks[i][j]->draw(g);
+	//	}
+	//}
+
+	// Select all blocks on the screen and draw them //
+	Block* upperLeft = &getBlockAt(window.box.a);
+	Block* lowerRight = &getBlockAt(window.box.b);
+
+	Point begin = Point(upperLeft->a.x / BLOCK_SIZE, upperLeft->a.y / BLOCK_SIZE);
+	Point end = Point(lowerRight->a.x / BLOCK_SIZE, lowerRight->a.y / BLOCK_SIZE);
+
+	for (unsigned int i = begin.x; i <= end.x; i++)
+	{
+		for (unsigned int j = begin.y; j <= end.y; j++)
+		{
+			blocks[i][j]->draw(g);
+		}
+	}
 }
 Block& World::getBlockAt(Point& p)
 {
-	//int x = p.x / BLOCK_SIZE;
-	//int y = p.y / BLOCK_SIZE;
-	//
-	//// Prevent index out of bounds
-	//if (p.x >= blocks.size()) x = blocks.size() - 1;
-	//if (p.y >= blocks[0].size()) y = blocks[0].size() - 1;
+	int x = p.x / BLOCK_SIZE;
+	int y = p.y / BLOCK_SIZE;
 	
-	return *blocks[p.x / BLOCK_SIZE][p.y / BLOCK_SIZE];
+	// Prevent index out of bounds
+	if (x >= blocks.size()) x = blocks.size() - 1;
+	if (y >= blocks[0].size()) y = blocks[0].size() - 1;
+	
+	return *blocks[x][y];
+	//return *blocks[p.x / BLOCK_SIZE][p.y / BLOCK_SIZE];
 }
