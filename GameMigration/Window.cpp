86ce8ecf0d56b45bafe3338226d8390bc4a2bbc9
Tyler Berry <tyler@berryac.com>
2015-05-01 19:17:36 -0600

    Now only the actual window has to move.

--- GameMigration/Window.cpp
+++ GameMigration/Window.cpp
@@ -25,19 +25,23 @@ void Window::shift(Vector& shift)
 {
 	box += shift;
 	deadzone += shift;
-	// update() ensures the boxes are in the right place
 	update();
 }
 
+// Ensure the boxes are in the correct place
 void Window::update()
 {
 	// The window box should only shift if the deadzone is not in the center
 	if (deadzone.a.x - box.a.x < WINDOW_WIDTH / 3) box += Vector(-(WINDOW_WIDTH / 3 - (deadzone.a.x - box.a.x)), 0);
 	else if (box.b.x - deadzone.b.x < WINDOW_WIDTH / 3) box += Vector((WINDOW_WIDTH / 3 - (box.a.x - deadzone.a.x)), 0);
+	if (deadzone.a.y - box.a.y < WINDOW_HEIGHT / 3) box += Vector(0, -(WINDOW_HEIGHT / 3 - (deadzone.a.y - box.a.y)));
+	else if (box.b.y - deadzone.b.y < WINDOW_HEIGHT / 3) box += Vector(0, (WINDOW_HEIGHT / 3 - (box.a.y - deadzone.a.y)));
 
 	// Don't let the window box go outside the world
 	if (box.a.x < 0) box += Vector(-box.a.x, 0);
 	else if (box.b.x > WORLD_WIDTH * BLOCK_SIZE) box += Vector(WORLD_WIDTH * BLOCK_SIZE - box.b.x, 0);
+	if (box.a.y < 0) box += Vector(0, -box.a.y);
+	else if (box.b.y > WORLD_HEIGHT * BLOCK_SIZE) box += Vector(0, WORLD_HEIGHT * BLOCK_SIZE - box.b.y);
  }
 void Window::draw(Core::Graphics& g)
 {
