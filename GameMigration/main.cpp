1a48a2d09cdd7ed531ab71f070da0eb04b0e0b2c
Tahler <tyler@berryac.com>
2015-05-05 15:06:39 -0600

    s

--- GameMigration/main.cpp
+++ GameMigration/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int invItems[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 Inventory Character::inventory = Inventory(10, invItems, 0);
 World world;
-Character player(&world, 4, 3);
+Character player(&world, 3, 3);
 
 ////////////////////////////////////////////////////////////
 
