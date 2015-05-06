5e2f6df45450c6d0c039de83237701dda2796bcb
Anglin16 <anglincarver@gmail.com>
2015-05-05 19:43:36 -0600

    Store progress

---
+++ GameMigration/Write.h
@@ -0,0 +1,9 @@
+#ifndef WRITE_H
+#define WRITE_H
+
+#include "World.h"
+#include "Physics.h"
+
+void writeMoney(Core::Graphics&, Point p, int, int, int);
+void drawZero(Core::Graphics&, Point p, int, int);
+#endif
